e74bab1638aa8eff7bc124ddf088ef1da2c769eb
Elwan <144356501+ElwanBeauchot@users.noreply.github.com>
2026-01-14 16:58:55 +0100

    url bdd

--- app/db.py
+++ app/db.py
@@ -1,6 +1,6 @@
 ﻿from motor.motor_asyncio import AsyncIOMotorClient
 
-MONGO_URL = "mongodb://localhost:27017"
+MONGO_URL = "mongodb+srv://elwanblb_db_user:3i4Ye1Whtw1PHLpX@shareup.e1ebvub.mongodb.net/?appName=ShareUp"
 
 client = AsyncIOMotorClient(MONGO_URL)
 db = client.share_up_db
