e9a6cb913ccebe767c823ef0d6f005d0e8640dde
ElwanBeauchot <elwan.beauchot-le-bon@edu.esiee-it.fr>
2026-03-22 20:58:32 +0100

    fix db tls

--- app/db.py
+++ app/db.py
@@ -1,6 +1,6 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from app.config import settings
 
-client = AsyncIOMotorClient(settings.mongo_url)
+client = AsyncIOMotorClient(settings.mongo_url, tls=True)
 db = client[settings.database_name]
 devices_collection = db[settings.devices_collection_name]
